ea84083cb2ba6115ae95b43d4a080acc593afde2
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2025-02-28 19:15:14 +0100

    Added core logic
    Added the core logic.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
 mod valkyrja;
 fn main() {
-    println!("Hello, world!");
+    let file_error = valkyrja::throw_file_error("File not found!");
+    let value_error = valkyrja::throw_value_error("Value not correct!");
+    let runtime_error = valkyrja::throw_runtime_error("Error during run!");
+    let unknown_error = valkyrja::throw_unkown_error("An unknown error occurred!");
+
+    if valkyrja::returns_exception(&file_error) {
+        println!("{}", valkyrja::get_error_message(file_error))
+    }
+
+    if valkyrja::returns_exception(&value_error) {
+        println!("{}", valkyrja::get_error_message(value_error))
+    }
+    
+    if valkyrja::returns_exception(&runtime_error) {
+        println!("{}", valkyrja::get_error_message(runtime_error))
+    }
+
+    if valkyrja::returns_exception(&unknown_error) {
+        println!("{}", valkyrja::get_error_message(unknown_error))
+    }
+
+    println!("{}", valkyrja::raise_info("Generation process started"));
+    println!("{}", valkyrja::raise_warning("Couldn't read value x from y!"));
 }
--- src/valkyrja/mod.rs
+++ src/valkyrja/mod.rs
@@ -3,13 +3,14 @@ pub struct Exception {
   code: ExceptionCode,
   message: String
 }
-
+#[derive(Copy, Clone)]
 enum ExceptionLevel {
   INFO,
   WARNING,
   ERROR
 }
 
+#[derive(Copy, Clone)]
 enum ExceptionCode {
   FileError,
   ValueError,
@@ -17,28 +18,55 @@ enum ExceptionCode {
   UnkownError
 }
 
-fn get_level(level: ExceptionLevel) -> i8 {
+fn get_exception_level(level: ExceptionLevel) -> String {
   match level {
-    ExceptionLevel::INFO => return 0,
-    ExceptionLevel::WARNING => return 1,
-    ExceptionLevel::ERROR => return 2
+    ExceptionLevel::INFO => return String::from("INFO"),
+    ExceptionLevel::ERROR => return String::from("ERROR"),
+    ExceptionLevel::WARNING => return String::from("WARNING")
   }
 }
 
-fn get_code(code: ExceptionCode) -> i8 {
+fn get_exception_code(code: ExceptionCode) -> String {
   match code {
-    ExceptionCode::FileError => return 0,
-    ExceptionCode::ValueError => return 1,
-    ExceptionCode::RuntimeError => return 2,
-    ExceptionCode::UnkownError => return 3
+    ExceptionCode::FileError => return String::from("FILE ERROR"),
+    ExceptionCode::ValueError => return String::from("VALUE ERROR"),
+    ExceptionCode::RuntimeError => return String::from("RUNTIME ERROR"),
+    ExceptionCode::UnkownError => return String::from("UNKOWN ERROR")
   }
 }
 
 fn create_exception(level: ExceptionLevel, code: ExceptionCode, message: &str) -> Exception {
-  return Exception{ level: level, code: code, message: message.to_string() }
+  return Exception{ level: level, code: code, message: String::from(message) }
 }
 
-pub fn returns_exception(e: Exception) -> bool {
-  return get_level(e.level) != -1 && get_code(e.code) != -1 && e.message != "".to_string()
+pub fn returns_exception(e: &Exception) -> bool {
+  return e.level as i8 != -1 && e.code as i8 != -1 && e.message != String::from("")
 }
 
+pub fn throw_file_error(message: &str) -> Exception {
+  return create_exception(ExceptionLevel::ERROR, ExceptionCode::FileError, message)
+}
+
+pub fn throw_value_error(message: &str) -> Exception {
+  return create_exception(ExceptionLevel::ERROR, ExceptionCode::ValueError, message)
+}
+
+pub fn throw_runtime_error(message: &str) -> Exception {
+  return create_exception(ExceptionLevel::ERROR, ExceptionCode::RuntimeError, message)
+}
+
+pub fn throw_unkown_error(message: &str) -> Exception {
+  return create_exception(ExceptionLevel::ERROR, ExceptionCode::UnkownError, message)
+}
+
+pub fn get_error_message(e: Exception) -> String {
+  return format!("{} - {}: {}", &get_exception_level(e.level), &get_exception_code(e.code), &e.message)
+}
+
+pub fn raise_info(message: &str) -> String {
+  return format!("{}: {}", &get_exception_level(ExceptionLevel::INFO), message)
+}
+
+pub fn raise_warning(message: &str) -> String {
+  return format!("{}: {}", &get_exception_level(ExceptionLevel::WARNING), message)
+}

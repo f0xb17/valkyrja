d971c21655fbcbaeeeabc741ba90baf5fbf4cb15
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2025-03-01 18:11:27 +0100

    Fixed a Typo
    Fixed a typo where unknown would be spelled wrong.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use valkyrja::{throw_file_exception, throw_runtime_exception, throw_unkown_exception, 
-                throw_value_exception, Exception, 
-                get_exception_message, raise_info, raise_warning};
+use valkyrja::{throw_file_exception, throw_runtime_exception, throw_unknown_exception,
+               throw_value_exception, Exception,
+               get_exception_message, raise_info, raise_warning};
 
 fn divide(x: u8, y: u8) -> Result<u8, Exception> {
     if y == 0 {
@@ -13,7 +13,7 @@ fn main() {
     let _file_error = throw_file_exception("File not found!");
     let _value_error = throw_value_exception(None);
     let _runtime_error = throw_runtime_exception("Error during run!");
-    let _unknown_error = throw_unkown_exception("An unknown error occurred!");
+    let _unknown_error = throw_unknown_exception("An unknown error occurred!");
 
     println!("{}", raise_info("Generation process started"));
     println!("{}", raise_warning("Couldn't read value x from y!"));

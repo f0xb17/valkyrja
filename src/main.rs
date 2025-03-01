0706b2a557aaefe2a0380859dfa2cbd880e56297
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2025-03-01 08:40:16 +0100

    Cleaned Code
    Cleaned Code by adding use

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
 mod valkyrja;
+use valkyrja::{throw_file_error, throw_runtime_error, throw_unkown_error, 
+                throw_value_error, Exception, 
+                get_error_message, raise_info, raise_warning};
 
-fn divide(x: u8, y: u8) -> Result<u8, valkyrja::Exception> {
+fn divide(x: u8, y: u8) -> Result<u8, Exception> {
     if y == 0 {
-        return Err(valkyrja::throw_value_error("Divison by Zero is not allowed!"));
+        return Err(throw_value_error("Divison by Zero is not allowed!"));
     }
     Ok(x / y)
 }
 
 fn main() {
-    let _file_error = valkyrja::throw_file_error("File not found!");
-    let _value_error = valkyrja::throw_value_error("Value not correct!");
-    let _runtime_error = valkyrja::throw_runtime_error("Error during run!");
-    let _unknown_error = valkyrja::throw_unkown_error("An unknown error occurred!");
+    let _file_error = throw_file_error("File not found!");
+    let _value_error = throw_value_error("Value not correct!");
+    let _runtime_error = throw_runtime_error("Error during run!");
+    let _unknown_error = throw_unkown_error("An unknown error occurred!");
 
-    println!("{}", valkyrja::raise_info("Generation process started"));
-    println!("{}", valkyrja::raise_warning("Couldn't read value x from y!"));
+    println!("{}", raise_info("Generation process started"));
+    println!("{}", raise_warning("Couldn't read value x from y!"));
 
     let result = divide(2,0 );
     match result {
         Ok(value) => println!("The Result ist: {}", value),
-        Err(e) => println!("{}", valkyrja::get_error_message(e))
+        Err(e) => println!("{}", get_error_message(e))
     }
 }
